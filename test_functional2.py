cd0b71a0e7aa11f968f0fc36fe36e748aa543cef
Gildardo Bautista <gil.cano@gmail.com>
2014-10-21 14:58:26 -0500

    complete test

--- test_functional2.py
+++ test_functional2.py
@@ -9,21 +9,32 @@ class WordCounterTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
     def test_index(self):
-        # Edith has heard about a cool new online app. She goes
+        # Gil has heard about a cool new online app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:5000')
         self.assertIn('Word Counter', self.browser.title)
 
+        # He is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('text')
-        self.assertEqual(inputbox.get_attribute('placeholder'), '')
 
-        inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.ENTER)
+        # He types "Facultad de Ciencias" into a text box
+        inputbox.send_keys('Facultad de Ciencias')
+
+        # When he hits enter, the page updates, and now the page lists
+        # number of characters and number of words
+        submit = self.browser.find_element_by_id('submit')
+        submit.send_keys(Keys.ENTER)
+
+        alert = self.browser.find_element_by_class_name('alert')
+        self.assertIn('3 palabras 20 caracteres', alert.text)
+
+        # self.browser.find_elements_by_tag_name('button')[0].text
 
 
 if __name__ == '__main__':
